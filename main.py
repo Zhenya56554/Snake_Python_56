c86ef7874c4a544c7921eb384692779d2076307f
Zhenya56554 <zhenya56555@gmail.com>
2024-06-09 01:47:46 +0300

    Создание игрового поля

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import random
 
 pygame.init()
 
-dis=pygame.display.set_mode((500,400))  #Задаём размер игрового поля.
+dis=pygame.display.set_mode((500,400))
 pygame.display.update()
 
+pygame.display.set_caption('Snake Game')
+ 
+game_over=False
+ 
+while not game_over:
+   for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+           game_over = True
+ 
 pygame.quit()
