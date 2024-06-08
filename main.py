20f986414a930fe09256a1e8fed646a8350dadd9
Zhenya56554 <zhenya56555@gmail.com>
2024-06-09 01:35:58 +0300

    Создание игрового поля

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from classes import *
 import pygame
 import pygame_menu
 import random
+
+pygame.init()
+
+dis=pygame.display.set_mode((500,400))  #Задаём размер игрового поля.
+pygame.display.update()
+
+pygame.quit()
